c69c380c29db23d1635f80da0d6d841825451647
Lucas1805 <khaithong08@gmail.com>
2016-09-07 16:02:14 +0700

    Fix lỗi login

--- Teek_AR/Assets/LoginScene.cs
+++ Teek_AR/Assets/LoginScene.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
+using UnityEngine.Networking;
 
 public class LoginScene : MonoBehaviour {
     public UnityEngine.UI.InputField usernameField;
     public UnityEngine.UI.InputField passwordField;
-    public int nextSceneID; 
+    public int nextSceneID;
+
+    private string url = "";
 
 	// Use this for initialization
 	void Start () {
@@ -29,45 +32,24 @@ public class LoginScene : MonoBehaviour {
         //REPLACE BELOW CODE WITH LOGIN API 
         //if (usernameField.text == "admin" && passwordField.text == "admin")
         //{
-        //    Start a coroutine that will load the desired scene.
+        //    //Start a coroutine that will load the desired scene.
         //    StartCoroutine(LoadNextScene());
         //    Debug.Log("Success");
-        //    Application.LoadLevel("pokemon");
         //}
 
 
-        string url = "10.255.250.160:19291/api/account/login/";
-
-        Dictionary<string, string> a = new Dictionary<string, string>();
-        POST(url,a);    
-        
-
-    }
-
-    public WWW POST(string url, Dictionary<string, string> post)
-    {
         WWWForm form = new WWWForm();
-        foreach (KeyValuePair<String, String> post_arg in post)
+        form.AddField("Username", username);
+        form.AddField("Password", password);
+
+        //SEND POST REQUEST
+        UnityWebRequest www = UnityWebRequest.Post(url,form);
+        www.Send();
+
+        //Check result
+        if(!www.isError) //If send request sucess
         {
-            form.AddField(post_arg.Key, post_arg.Value);
-        }
-        WWW www = new WWW(url, form);
-
-        StartCoroutine(WaitForRequest(www));
-        return www;
-    }
-
-    private IEnumerator WaitForRequest(WWW www)
-    {
-        yield return www;
-
-        // check for errors
-        if (www.error == null)
-        {
-            Debug.Log("WWW Ok!: " + www.text);
-        }
-        else {
-            Debug.Log("WWW Error: " + www.error);
+            Debug.Log(www.downloadHandler.text);
         }
     }
 
