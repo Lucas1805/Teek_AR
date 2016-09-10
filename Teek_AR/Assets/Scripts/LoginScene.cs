f6e50ef58b3f13a97b48ebb0df47ccc7bbb2b9ca
Lucas1805 <khaithong08@gmail.com>
2016-09-10 16:25:10 +0700

    Khóa xoay màn hình, chỉnh sửa lại 2 scene Login và Register, validate 1 số trường hợp trong register. Sắp xpe61 lại structure trong thư mục

---
+++ Teek_AR/Assets/Scripts/LoginScene.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using LitJson;
+
+public class LoginScene : MonoBehaviour {
+    public UnityEngine.UI.InputField usernameField;
+    public UnityEngine.UI.InputField passwordField;
+    public Text message;
+    public int nextSceneID;
+
+    private string url = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+
+    public void checkLogin()
+    {
+        message.text = "";
+        //Get value from input fields
+        string username = usernameField.text;
+        string password = passwordField.text;
+
+        //Check if player enter any iformation. If not do nothing
+        if (username.Length > 0 && password.Length > 0)
+        {
+            //Check if Login Infomartion is valid or not
+            //REPLACE BELOW CODE WITH LOGIN API 
+            if (usernameField.text == "admin" && passwordField.text == "admin")
+            {
+                //Start a coroutine that will load the desired scene.
+                SceneManager.LoadSceneAsync(nextSceneID);
+            }
+
+            //Create object to sen Http Request
+            WWWForm form = new WWWForm();
+            form.AddField("Username", username);
+            form.AddField("Password", password);
+
+            //SEND POST REQUEST
+            UnityWebRequest www = UnityWebRequest.Post(url, form);
+            www.Send();
+
+            //Check result
+            if (!www.isError) //If send request sucess
+            {
+                BuuCLass person = new BuuCLass();
+
+
+                person = JsonMapper.ToObject<BuuCLass>(www.downloadHandler.text);
+                Debug.Log(person.Succeed + " " + person.Message);
+
+            }
+        }
+        else
+        {
+            message.text = "Please enter username and password";
+        }
+    }
+    [System.Serializable]
+    public class BuuCLass
+    {
+
+        public bool Succeed { get; set; }
+
+        public string Message { get; set; }
+
+        public string Errors { get; set; }
+        public string Data { get; set; }
+       
+    }
+    public void resetField()
+    {
+        usernameField.text = "";
+        passwordField.text = "";
+    }
+    
+}
---
+++ Teek_AR/Assets/Scripts/RegisterScript.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class RegisterScript : MonoBehaviour {
+    public UnityEngine.UI.InputField fullnameField;
+    public UnityEngine.UI.InputField usernameField;
+    public UnityEngine.UI.InputField emailField;
+    public UnityEngine.UI.InputField passwordField;
+    public UnityEngine.UI.InputField passwordAgainField;
+    public Text message;
+
+    string fullname;
+    string username;
+    string email;
+    string password;
+    string passwordAgain;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void resetField()
+    {
+        fullnameField.text = "";
+        usernameField.text = "";
+        emailField.text = "";
+        passwordField.text = "";
+        passwordAgainField.text = "";
+        message.text = "";
+    }
+
+    public void doRegister()
+    {
+        //Reset message
+        message.text = "";
+        
+        //Get values
+        fullname = fullnameField.text;
+        email = emailField.text;
+        username = usernameField.text;
+        password = passwordField.text;
+        passwordAgain = passwordAgainField.text;
+
+        if(checkRequireInfo())
+        {
+            
+        }
+    }
+
+    private bool validateEmail(string email)
+    {
+        var regex = new Regex(@"[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        return regex.IsMatch(email);
+    }
+
+    private bool checkRequireInfo()
+    {
+        bool result = true;
+        if (fullname.Length <= 0)
+        {
+            result = false;
+            fullnameField.Select();
+            message.text = "Please enter full name";
+        }
+        else if (email.Length <= 0)
+        {
+            result = false;
+            emailField.Select();
+            message.text = "Please enter email";
+        }
+        else if (email.Length > 0 && !validateEmail(email))
+        {
+            result = false;
+            emailField.Select();
+            message.text = "Email format is not valid";
+
+        }
+        else if (username.Length <= 0)
+        {
+            result = false;
+            usernameField.Select();
+            message.text = "Please enter your username";
+        }
+        else if (password.Length <= 0)
+        {
+            result = false;
+            passwordField.Select();
+            message.text = "Please enter your password";
+        }
+        else if(!password.Equals(passwordAgain))
+        {
+            result = false;
+            passwordAgainField.Select();
+            message.text = "Re-enter Password is not match";
+        }
+        return result;
+    }
+
+
+}
