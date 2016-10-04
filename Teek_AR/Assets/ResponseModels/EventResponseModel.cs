8e5faa4f7613160720ae79cb1606b7ad27cd29ba
treasuryonly <treasuryonly@gmail.com>
2016-10-04 14:24:18 +0700

    thêm ResponseModel với EventResponseModel

---
+++ Teek_AR/Assets/ResponseModels/EventResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.ResponseModels
+{
+    class EventResponseModel
+    {
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Teek_AR/Assets/ResponseModels/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.ResponseModels
+{
+    class ResponseModel<T> where T:class
+    {
+        public bool Succeed { get; set; }
+        public string Message { get; set; }
+        public string Errors { get; set; }
+        public T Data { get; set; }
+
+    }
+}
