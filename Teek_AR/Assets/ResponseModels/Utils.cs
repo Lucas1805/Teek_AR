3c050984e4cde8e24a4495c15904f9d75d6729bc
Frost <Frost>
2016-10-05 13:30:33 +0700

    Them file utils

--- Teek_AR/Assets/ResponseModels/Utils.cs
+++ Teek_AR/Assets/ResponseModels/Utils.cs
@@ -7,5 +7,24 @@ namespace Assets.ResponseModels
 {
     class Utils
     {
+        public DateTimeOffset GetDateTimeOffset(string inDate)
+        {
+            string delimiter = "-";
+
+            if (inDate.IndexOf("+") != -1)
+            {
+                delimiter = "+";
+            }
+
+            string[] dateParts = inDate.Split(new string[] { delimiter }, 2, System.StringSplitOptions.None);
+
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Unspecified);
+
+            var parsedDate = epoch.AddMilliseconds(Convert.ToDouble(dateParts[0]));
+
+            var offset = TimeSpan.ParseExact(dateParts[1], "hhmm", null, delimiter == "-" ? System.Globalization.TimeSpanStyles.AssumeNegative : System.Globalization.TimeSpanStyles.None);
+
+            return new DateTimeOffset(parsedDate, offset);
+        }
     }
 }
