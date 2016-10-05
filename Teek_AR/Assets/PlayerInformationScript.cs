50a9a8686cca8e6d656d388307c5c08416c54202
sabufung <phungvan_buu@yahoo.com.vn>
2016-10-05 13:27:17 +0700

    Event Detail and Player Info Scene Initialization

---
+++ Teek_AR/Assets/PlayerInformationScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerInformationScript : MonoBehaviour {
+
+    //Use these 2 Key string to store login session in PlayerPrefs
+    private readonly string usernameKeyValue = "Username";
+    private readonly string passwordKeyValue = "Pass";
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Logout()
+    {
+        PlayerPrefs.DeleteKey(usernameKeyValue);
+        PlayerPrefs.DeleteKey(passwordKeyValue);
+        SceneManager.LoadSceneAsync("LoginScene");
+    }
+}
