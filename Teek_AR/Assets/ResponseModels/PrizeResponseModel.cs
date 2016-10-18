92a60c3d77231b4c4e6ae96c6d3bdf39fce9235b
Lucas1805 <khaithong08@gmail.com>
2016-10-18 13:53:32 +0700

    Model để hứng Response

---
+++ Teek_AR/Assets/ResponseModels/PrizeResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.ResponseModels
+{
+    public partial class PrizeResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int EventId { get; set; }
+        public bool Active { get; set; }
+        public int Teek { get; set; }
+        public int Ruby { get; set; }
+        public int Sapphire { get; set; }
+        public int Citrine { get; set; }
+    }
+}
