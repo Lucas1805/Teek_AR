44f496520324f0fc1756ebf13312c8adde70d33c
sabufung <phungvan_buu@yahoo.com.vn>
2016-10-20 09:51:28 +0700

    Chỉnh lại prefab vô resource để load cho dễ và chỉnh UI Dialog

---
+++ Teek_AR/Assets/Helpers/DialogTemplate.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogTemplate : MonoBehaviour
+{
+    public Text Message;
+    public GameObject No;
+    public GameObject Yes;
+
+}
+
