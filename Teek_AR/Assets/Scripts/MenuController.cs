c599b8e66bb49bfed2b7cf1f78b4af0a31b50547
sabufung <phungvan_buu@yahoo.com.vn>
2016-10-23 23:09:42 +0700

    Thêm animation cho menu

--- Teek_AR/Assets/Scripts/MenuController.cs
+++ Teek_AR/Assets/Scripts/MenuController.cs
@@ -5,15 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private bool isOpen = false;
+    private bool isClose = false;
+    private Vector2 origin;
+    private Vector2 target;
+
+    // Use this for initialization
+    void Start () {
+        origin = transform.position;
+        target.y = transform.position.y;
+        target.x = transform.position.x + 860;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (isOpen)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, 3000 * Time.deltaTime);
+
+        }
+        if (isClose)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, origin, 3000 * Time.deltaTime);
+        }
+    }
 
     public void Logout()
     {
@@ -34,5 +49,16 @@ public class MenuController : MonoBehaviour {
     public void NavigateCouponScene()
     {
         SceneManager.LoadSceneAsync(ConstantClass.CouponSceneName);
+    }
+
+    public void Open()
+    {
+        isOpen = true;
+        isClose = false;
+    }
+    public void Close()
+    {
+        isOpen = false;
+        isClose = true;
     }
 }
