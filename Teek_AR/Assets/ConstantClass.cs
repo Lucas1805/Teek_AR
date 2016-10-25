a7aa68aaa1382800062c39796bc82018fe7ab6f1
sabufung <phungvan_buu@yahoo.com.vn>
2016-10-25 23:18:05 +0700

    thêm couponscene - fix UI code prize success panel and code prize template

--- Teek_AR/Assets/ConstantClass.cs
+++ Teek_AR/Assets/ConstantClass.cs
@@ -45,7 +45,7 @@ namespace Assets
 
         #region API LINKS
         //API LINKS
-        public const string API_HOST_IP = "192.168.1.8";
+        public const string API_HOST_IP = "localhost";
         public const string API_Login = "http://" + API_HOST_IP + "/Teek/api/account/login";
         public const string API_Register = "http://" + API_HOST_IP + "/Teek/api/account/register";
         public const string API_RedeemPrizeCode = "http://" + API_HOST_IP + "/Teek/api/Prize/ReedemPrize";
