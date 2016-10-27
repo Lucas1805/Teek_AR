3bd49f020f73221d517831e75f71878d51a2abf0
Lucas1805 <khaithong08@gmail.com>
2016-10-27 14:09:23 +0700

    Add lại ShopController

---
+++ Teek_AR/Assets/Scripts/ShopController.cs
@@ -0,0 +1,277 @@
+﻿using UnityEngine;
+using System.Collections;
+using MobyShop;
+using MobyShop.UI;
+using UnityEngine.SceneManagement;
+using Assets;
+using LitJson;
+using Assets.ResponseModels;
+using Ucss;
+
+public class ShopController : MonoBehaviour {
+
+    public ShopUIBase mobyShop;
+    public GameObject loadingPanel;
+    public GameObject menuGroupButtonPanel;
+    public static int GameId;
+    
+    private int coinNumber = 666;
+    private int fireballNumber = 13;
+    private int iceballNumber = 13;
+    
+    private ProductInfo justBoughtProduct;
+
+    // Use this for initialization
+    void Start () {
+
+        //CALL AIP TO GET NUMBER OF ITEM OF USER TO LOAD
+        LoadCustomerInformation();
+        Shop.OnProductBought += this.OnProductBought;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void showShop()
+    {
+        mobyShop.Show(0,null);
+    }
+
+    public void setValueToShop()
+    {
+        Shop.ClearAllPurchaseData();
+
+        ProductInfo fireball = Shop.GetProduct(ConstantClass.FireBallItemID);
+        ProductInfo coin = Shop.GetProduct(ConstantClass.CoinItemID);
+        ProductInfo iceball = Shop.GetProduct(ConstantClass.IceBallItemID);
+
+        //SET VALUE
+        fireball.Value = fireballNumber;
+        coin.Value = coinNumber;
+        iceball.Value = iceballNumber;
+
+    }
+        
+    void OnProductBought( MobyShop.BoughtOrRestored state, MobyShop.ProductInfo product, int amount)
+    {
+        //Show loading
+        LoadingManager.showLoadingIndicator(loadingPanel);
+        justBoughtProduct = product;
+
+        //CALL API TO UPDATE VALUE OF PRODUCT IN SERVER DATABASE
+        //Create object to send Http Request
+        HTTPRequest request = new HTTPRequest();
+        WWWForm form = new WWWForm();
+        
+        if (product.ProductClass.Equals(ConstantClass.CoinItemClassName))
+        {
+            form.AddField("userId", Decrypt.DecryptString(PlayerPrefs.GetString(ConstantClass.PP_UserIDKey)));
+            form.AddField("coinAmount", product.IncrementOnBuy);
+
+            request.url = ConstantClass.API_UpdateCoinItem;
+
+            request.stringCallback = new EventHandlerHTTPString(this.OnDoneCallBuyCoinRequest);
+        }
+        else
+        {
+            form.AddField("userId", Decrypt.DecryptString(PlayerPrefs.GetString(ConstantClass.PP_UserIDKey)));
+            form.AddField("organizerId", PlayerPrefs.GetInt(ConstantClass.PP_OrganizerId));
+            form.AddField("price", product.price);
+
+            request.url = ConstantClass.API_UpdateBallItem;
+
+            if (product.ProductClass.Equals(ConstantClass.FireBallItemClassName)) //If player buy Fireball
+            {
+                form.AddField("fireballAmount", product.IncrementOnBuy);
+                form.AddField("iceballAmount", 0);
+            }
+
+            if (product.ProductClass.Equals(ConstantClass.IceBallItemClassName)) //If player buy Iceball
+            {
+                form.AddField("fireballAmount", 0);
+                form.AddField("iceballAmount", product.IncrementOnBuy);
+            }
+
+            request.stringCallback = new EventHandlerHTTPString(this.OnDoneCallBuyItemRequest);
+            
+        }
+
+        request.onTimeOut = new EventHandlerServiceTimeOut(this.OnTimeOut);
+        request.onError = new EventHandlerServiceError(this.OnBuyItemError);
+
+        request.formData = form;
+
+        UCSS.HTTP.PostForm(request);
+    }
+    
+    private void restorePurchase(ProductInfo product, int deviation)
+    {
+        //Restore product value
+        product.Value = product.Value - deviation;
+
+        //Restore teek value
+        Shop.GetProduct(ConstantClass.CoinItemID).Value = Shop.GetProduct(ConstantClass.CoinItemID).Value + product.price;
+    }
+
+    public void showHideMenuGroup()
+    {
+        if (menuGroupButtonPanel.active)
+            menuGroupButtonPanel.SetActive(false);
+        else
+            menuGroupButtonPanel.SetActive(true);        
+    }
+
+    
+
+    #region PROCESS BUY ITEM REQUEST
+    private void OnDoneCallBuyItemRequest(string result, string transactionId)
+    {
+        ResponseModel<CustomerResponseModel> jsonResponse = new ResponseModel<CustomerResponseModel>();
+        jsonResponse.Data = new CustomerResponseModel();
+        jsonResponse = JsonMapper.ToObject<ResponseModel<CustomerResponseModel>>(result);
+
+        if (jsonResponse.Succeed)
+        {
+            coinNumber = (int) jsonResponse.Data.Coin;
+            fireballNumber = (int)jsonResponse.Data.Fireball;
+            iceballNumber = (int)jsonResponse.Data.Iceball;
+
+            setValueToShop();
+
+        }
+        else
+        {
+            restorePurchase(justBoughtProduct, justBoughtProduct.IncrementOnBuy);
+            MessageHelper.MessageDialog("Error", jsonResponse.Message);
+            Debug.Log(jsonResponse.Message);
+        }
+        LoadingManager.hideLoadingIndicator(loadingPanel);        
+    }
+
+    private void OnDoneCallBuyCoinRequest(string result, string transactionId)
+    {
+        ResponseModel<CustomerResponseModel> jsonResponse = new ResponseModel<CustomerResponseModel>();
+        jsonResponse.Data = new CustomerResponseModel();
+        jsonResponse = JsonMapper.ToObject<ResponseModel<CustomerResponseModel>>(result);
+
+        if (jsonResponse.Succeed)
+        {
+            coinNumber = (int) jsonResponse.Data.Coin;
+            setValueToShop();
+        }
+        else
+        {
+            restorePurchase(justBoughtProduct, justBoughtProduct.IncrementOnBuy);
+            MessageHelper.MessageDialog("Error", jsonResponse.Message);
+            Debug.Log(jsonResponse.Message);
+        }
+        LoadingManager.hideLoadingIndicator(loadingPanel);
+    }
+
+    private void OnBuyItemError(string error, string transactionId)
+    {
+        MessageHelper.MessageDialog(error);
+
+        //RESTORE PURCHASE
+        restorePurchase(justBoughtProduct, justBoughtProduct.IncrementOnBuy);
+
+        Debug.Log(error);
+        
+    }
+    #endregion
+    
+ 
+
+    private void OnTimeOut(string transactionId)
+    {
+        //showLoginMessage(ConstantClass.Msg_TimeOut);
+        MessageHelper.MessageDialog(ConstantClass.Msg_TimeOut);
+        Debug.Log(ConstantClass.Msg_TimeOut);
+        
+    }
+
+    #region LOAD OTHER SCENE
+
+    public void loadRedeemCodeScene()
+    {
+        
+        //SET LAST SCENE VALUE BEFORE LOAD NEXT SCENE
+        MySceneManager.setLastScene(ConstantClass.GameSceneName);
+
+        SceneManager.LoadSceneAsync(ConstantClass.RedeemCodeSceneName);
+    }
+
+    public void loadHomeScene()
+    {
+        
+        //SET LAST SCENE VALUE BEFORE LOAD NEXT SCENE
+        MySceneManager.setLastScene(ConstantClass.GameSceneName);
+
+        SceneManager.LoadSceneAsync(ConstantClass.HomeSceneName);
+    }
+
+    public void loadInventoryScene()
+    {
+        
+        //SET LAST SCENE VALUE BEFORE LOAD NEXT SCENE
+        MySceneManager.setLastScene(ConstantClass.GameSceneName);
+
+        SceneManager.LoadSceneAsync(ConstantClass.InventorySceneName);
+    }
+    #endregion
+
+    public void LoadCustomerInformation()
+    {
+        LoadingManager.showLoadingIndicator(loadingPanel);
+        HTTPRequest request = new HTTPRequest();
+        request.url = ConstantClass.API_LoadCustomerInformation;
+
+        WWWForm form = new WWWForm();
+        form.AddField("userId", Decrypt.DecryptString(PlayerPrefs.GetString(ConstantClass.PP_UserIDKey)));
+        form.AddField("organizerId", PlayerPrefs.GetInt(ConstantClass.PP_OrganizerId));
+
+        request.formData = form;
+
+        request.stringCallback = new EventHandlerHTTPString(this.OnDoneCustomerInformationRequest);
+        request.onTimeOut = new EventHandlerServiceTimeOut(MessageHelper.OnTimeOut);
+        request.onError = new EventHandlerServiceError(MessageHelper.OnError);
+
+        UCSS.HTTP.PostForm(request);
+    }
+
+    #region PROCESS LOAD USER INFORMATION REQUEST
+    private void OnDoneCustomerInformationRequest(string result, string transactionId)
+    {
+        ResponseModel<CustomerResponseModel> jsonResponse = new ResponseModel<CustomerResponseModel>();
+        jsonResponse.Data = new CustomerResponseModel();
+        jsonResponse = JsonMapper.ToObject<ResponseModel<CustomerResponseModel>>(result);
+
+        if (jsonResponse.Succeed)
+        {
+            if(jsonResponse.Data.Coin != null)
+            {
+                coinNumber = (int)jsonResponse.Data.Coin;
+            }
+            if (jsonResponse.Data.Fireball != null)
+            {
+                fireballNumber = (int) jsonResponse.Data.Fireball;
+            }
+            if (jsonResponse.Data.Iceball != null)
+            {
+                iceballNumber = (int)jsonResponse.Data.Iceball;
+            }
+        }
+        else
+        {
+            //Show error message
+            MessageHelper.MessageDialog(jsonResponse.Message);
+        }
+
+        //Call Set Value To Shop After Getting Data From Server
+        setValueToShop();
+        LoadingManager.hideLoadingIndicator(loadingPanel);
+    }
+    #endregion
+}
